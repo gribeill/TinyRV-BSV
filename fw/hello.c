63e8b631f016419594a4e8688dc5fcf3bbcd5d85
Guilhem Ribeill <gribeill@bbn.com>
2021-09-22 21:02:42 -0400

    Update linker script

--- fw/hello.c
+++ fw/hello.c
@@ -1,9 +1,16 @@
+int doubler(int a) {
+	return 2*a;
+}
+
 int main() {
 	int a = 4;
 	int b = 7;
 	int c = 0;
+	int z = 0;
 	for (int i = 0; i < 3; i++){
-		c += i+a+b;
+		c += i+doubler(a)+b;
 	}
+	z = c;
+	asm volatile("ecall");
 	return 0;
 }
